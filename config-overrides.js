094939f9c5531dc21f1c616cde85327a835ba762
paulreitz <avenose@gmail.com>
2021-03-24 15:13:34 -0700

    Added to the config to not use live reload when the DB changes.

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,12 @@
 module.exports = function override(config, env) {
     config.target = 'electron-renderer';
+    if (env === 'development') {
+        const pluginIndex = config.plugins.findIndex(e => {
+            return e.constructor.name === 'ReactRefreshPlugin';
+        });
+        if (pluginIndex > -1) {
+            config.plugins[pluginIndex].options.exclude = [/node_modules/i, /public/i];
+        }
+    }
     return config;
 }
